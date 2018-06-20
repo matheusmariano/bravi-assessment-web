1236a027bbf0d702cd82bf9cfbfa636e390793f9
Matheus Mariano <vmatheus.mariano@gmail.com>
2018-06-19 23:52:35 -0300

    Format Not Found screen

--- app/screens/NotFound/index.jsx
+++ app/screens/NotFound/index.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { FormattedMessage } from 'react-intl';
 import Image from 'react-retina-image';
-import './styles.scss';
 
 export default class NotFoundScreen extends Component {
   static get contextTypes() {
@@ -21,18 +20,23 @@ export default class NotFoundScreen extends Component {
 
   render() {
     return (
-      <section styleName="not-found-page">
-        <FormattedMessage id="not_found.page_title">
-          {message => (
-            <Image
-              alt={message}
-              src="images/not-found.png"
-            />
-          )}
-        </FormattedMessage>
-        <h2>
-          <FormattedMessage id="not_found.title" />
-        </h2>
+      <section className="container">
+        <div className="row">
+          <div className="col text-center">
+            <FormattedMessage id="not_found.page_title">
+              {message => (
+                <Image
+                  alt={message}
+                  className="my-3"
+                  src="images/not-found.png"
+                />
+              )}
+            </FormattedMessage>
+            <h1>
+              <FormattedMessage id="not_found.title" />
+            </h1>
+          </div>
+        </div>
       </section>
     );
   }
